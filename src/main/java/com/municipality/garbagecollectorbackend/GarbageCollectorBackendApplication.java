aacd1f85235c6b1423f248902da900183c6f23ba
Omar Safi <safiomar.2003@gmail.com>
2025-11-18 18:10:39 +0100

    Initial commit

---
+++ src/main/java/com/municipality/garbagecollectorbackend/GarbageCollectorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.municipality.garbagecollectorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageCollectorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarbageCollectorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/municipality/garbagecollectorbackend/GarbageCollectorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.municipality.garbagecollectorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageCollectorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
