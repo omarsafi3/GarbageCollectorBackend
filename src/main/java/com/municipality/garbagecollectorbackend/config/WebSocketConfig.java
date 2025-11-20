a623da89a7a5714341d74fe4c350f6a28862cc40
Omar Safi <safiomar.2003@gmail.com>
2025-11-20 16:46:58 +0100

    WebSocketConfig adjusted

--- src/main/java/com/municipality/garbagecollectorbackend/config/WebSocketConfig.java
+++ src/main/java/com/municipality/garbagecollectorbackend/config/WebSocketConfig.java
@@ -19,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*") // <- matches your CORS config
                 .withSockJS();
+
     }
 }
